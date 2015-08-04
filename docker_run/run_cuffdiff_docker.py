163e1e8100e4ec3db48782762b8b47e22432f0f7
dmccloskey <dmccloskey87@gmail.com>
2015-08-04 14:54:59 -0700

    updates

--- docker_run/run_cuffdiff_docker.py
+++ docker_run/run_cuffdiff_docker.py
@@ -46,17 +46,17 @@ def run_cuffdiff_docker(samples_host_dir_1,samples_host_dir_2,samples_name_1,sam
     docker_name_dir_2 = [];
     for sample in samples_host_dir_1.split(','):
         samples_mount += "-v " + sample + ":" + docker_mount_1 + " ";
-        docker_name_dir_1.append(docker_mount_1 + '/' + sample.split('/')[-1])
+        docker_name_dir_1.append(docker_mount_1 + sample.split('/')[-1])
     for sample in samples_host_dir_2.split(','):
         samples_mount += "-v " + sample + ":" + docker_mount_1 + " ";
-        docker_name_dir_2.append(docker_mount_1 + '/' + sample.split('/')[-1])
+        docker_name_dir_2.append(docker_mount_1 + sample.split('/')[-1])
     samples_mount = samples_mount[:-1];
 
     if not more_options:
         more_options = 'None';
 
     rnaseq_cmd = ("run_cuffdiff('%s','%s','%s','%s','%s','%s','%s',threads=%s,library_norm_method=%s,fdr=%s,library_type=%s,more_options=%s);" \
-        %(docker_name_dir_1,docker_name_dir_2,samples_name_1,samples_name_2,\
+        %(str(docker_name_dir_1),str(docker_name_dir_2),samples_name_1,samples_name_2,\
         organism_I,user_output,docker_mount_2,threads,library_norm_method,fdr,library_type,more_options));
     python_cmd = ("from sequencing_utilities.cuffdiff import run_cuffdiff;%s" %(rnaseq_cmd));
     docker_run = ('sudo docker run --name=%s %s -v %s:%s dmccloskey/sequencing_utilities python3 -c "%s"' %(container_name,samples_mount,host_indexes_dir_I,docker_mount_2,python_cmd));
